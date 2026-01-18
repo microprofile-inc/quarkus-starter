af40bf965e4a990205b2089e889203d88840fafd
foxsir <838394225@qq.com>
2026-01-18 18:06:25 +0800

    remove redundancy code

--- src/main/java/com/microprofile/payload/SendSmsResponseAliyun.java
+++
@@ -1,8 +0,0 @@
-package com.microprofile.payload;
-
-import com.aliyun.dysmsapi20170525.models.SendSmsResponse;
-import org.eclipse.microprofile.openapi.annotations.media.Schema;
-
-@Schema
-public class SendSmsResponseAliyun extends SendSmsResponse {
-}
